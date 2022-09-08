8edd9ddb2d0023b191d86d7fb55cc9096daeaeb9
Andre Seilhan <seilhanmagnino@gmail.com>
2022-09-08 11:01:46 +0200

    add page error404

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom'
+
 function Error() {
-  return <div></div>
+  return (
+    <div className="error404">
+      <p className="error">404</p>
+      <p className="textInfo">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="text">
+        Retourner sur la page d’accueil
+      </Link>
+    </div>
+  )
 }
 
 export default Error
