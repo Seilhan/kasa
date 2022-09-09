d1bb0dc744e7520b2247cfd4912de619b21950b8
Andre Seilhan <seilhanmagnino@gmail.com>
2022-09-09 14:44:36 +0200

    add button description and equipements

---
+++ src/Components/Accordion/index.jsx
@@ -0,0 +1,7 @@
+import './style.css'
+
+function Drop() {
+  return <div></div>
+}
+
+export default Drop
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -4,6 +4,7 @@ import PictureProduct from '../Components/PictureProducts'
 import Tag from '../Components/Tag'
 import Rate from '../Components/Rate'
 import Host from '../Components/Host'
+// import Drop from '../Components/Accordion'
 
 function Product() {
   const { id } = useParams()
@@ -28,6 +29,10 @@ function Product() {
           <Host data={product.host} />
           <Rate nb={product.rating} />
         </div>
+      </div>
+      <div className="dropdown">
+        <button className="description">Description</button>
+        <button className="description">Equipements</button>
       </div>
     </div>
   )
