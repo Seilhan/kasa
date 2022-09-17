f8537aeac335faaa8f3d6f100b3ab42c453e1227
seilhan <seilhanmagnino@gmail.com>
2022-09-17 17:46:18 +0200

    Add page about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,14 +2,28 @@ import Drop from '../Components/Accordion'
 import PhotoHead from '../Components/PhotoHead'
 import AboutCover from '../assets/about-cover.jpg'
 
-function About() {
+function About(props) {
   return (
-    <div>
+    <div className="about-content">
       <PhotoHead img={AboutCover} />
-      <Drop title="Fiabilité" />
-      <Drop title="Respect" />
-      <Drop title="Service" />
-      <Drop title="Responsabilité" />
+      <div className="about-collapse">
+        <Drop
+          title="Fiabilité"
+          description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+        />
+        <Drop
+          title="Respect"
+          description="Les annonces La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme. sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+        />
+        <Drop
+          title="Service"
+          description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+        />
+        <Drop
+          title="Sécurité"
+          description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+        />
+      </div>
     </div>
   )
 }
