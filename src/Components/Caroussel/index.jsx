b33102a9764a09deba4a22c0d6110df260efbc74
Andre Seilhan <seilhanmagnino@gmail.com>
2022-09-25 08:58:20 +0200

    add function to the caroussel

--- src/Components/Caroussel/index.jsx
+++ src/Components/Caroussel/index.jsx
@@ -6,10 +6,29 @@ import './style.css'
 function Caroussel(props) {
   const URL = props.pictures ? props.pictures : []
 
+  const [imageIdx, setImageIdx] = useState(0)
   const [image, setImage] = useState(URL[0])
 
   function setNextImage() {
-    setImage()
+    setImageIdx(imageIdx + 1)
+    setImage(URL[imageIdx])
+
+    let idx = imageIdx + 1
+    if (idx >= URL.length()) idx = 0
+    setImageIdx(idx)
+    setImage(URL[imageIdx])
+  }
+
+  function setPrevImage() {
+    setImageIdx(imageIdx - 1)
+    setImage(URL[imageIdx])
+
+    let idx = imageIdx - 1
+    if (idx < 0) {
+      idx = URL.length - 1
+      setImageIdx(idx)
+      setImage(URL[imageIdx])
+    }
   }
 
   return (
@@ -17,7 +36,7 @@ function Caroussel(props) {
       <img className="caroussel-img" src={image} alt="cover" />
       <div className="caroussel-action">
         <div className="caroussel-arrow">
-          <button onClick={setNextImage}>
+          <button onClick={setPrevImage}>
             <ArrowCarousselL />
           </button>
           <button onClick={setNextImage}>
